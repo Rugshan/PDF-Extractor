af14a18a03026589029acb714624d8394ca56f87
Rugshan Gnanandram <rugshan@hotmail.com>
2022-07-21 02:47:36 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+# Imports
+import tkinter as tk
+import PyPDF2
+from PIL import Image, ImageTk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3)
+
+# Logo
+image = Image.open()
+
+root.mainloop()
